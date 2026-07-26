b7e8fd0e662a5af7e3bbe4c2b1fc493d8f9e3cb3
chaelin <alwaysriddle@naver.com>
2026-07-26 18:11:17 +0900

    feat(document): StoredFile 도메인과 FileStorage port 추가

---
+++ src/main/java/com/fowoco/server/document/domain/ScanStatus.java
@@ -0,0 +1,10 @@
+package com.fowoco.server.document.domain;
+
+/**
+ * 악성파일 검사 상태.
+ * 지금은 NOT_SCANNED로 고정하고 파일 연결은 허용한다. 실제 검증은 후속 이슈에서
+ * 검사 인프라가 구축된 뒤 CLEAN/INFECTED 등의 실제 판정값을 채우게 될 것이다.
+ */
+public enum ScanStatus {
+    NOT_SCANNED
+}
